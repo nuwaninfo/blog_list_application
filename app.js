540eead2975598be90cebaefa1354c3ece7ea7d2
Nuwan Fernando <nuwaninfo@gmail.com>
2024-02-16 22:09:07 +0200

    Complete 4.18: Blog List Expansion, step 6

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -21,6 +22,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
